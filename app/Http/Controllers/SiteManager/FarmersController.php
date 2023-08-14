268c06e23cd61220255e17e141290d8567a3e676
Lambertn33 <lambertgithub@gmail.com>
2023-08-14 12:36:10 +0200

    feat: rename land types

--- app/Http/Controllers/SiteManager/FarmersController.php
+++ app/Http/Controllers/SiteManager/FarmersController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class FarmersController extends Controller
 {
-    public function create(Request $request)
+    public function store(Request $request)
     {
         $request->validate([
             //users field
